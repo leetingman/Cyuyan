5d83bf09c19f195cc0a13b815012b78a4f216315
leetingman <dwa08@naver.com>
2022-06-11 15:39:33 +0900

    set env

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+  기말 평균 — finalScore , 
+  반평균 -- classScore,
+  간부 여부 — isOfficer,
+  서부 여부 — isWest
+  논문 여부 — isThesis
+*/
+
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
+
